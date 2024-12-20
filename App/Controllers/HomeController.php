a351971c8768e04b3b632550070b601c305a9768
LS <sochorova2@stud.uniza.sk>
2024-12-20 17:36:32 +0100

    completed home page

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -23,24 +23,29 @@ class HomeController extends AControllerBase
     {
         $whereClause = NULL;
         $whereParams = [];
+        $formData = $this->app->getRequest()->getPost();
+
+        $arr = [];
 
         $typeInt = $this->request()->getValue('type');
-        if (isset($typeInt)) {
+        //$typeInt = $formData['type'] ?? null;
+        if (isset($typeInt) && $typeInt != "") {
+            //$typeInt = filter_var($formData['type'], FILTER_VALIDATE_INT, [ "options" => ["min_range" => 0],"flags" => FILTER_NULL_ON_FAILURE]);
             $typeInt = filter_var($this->request()->getValue('type'), FILTER_VALIDATE_INT, [ "options" => ["min_range" => 0],"flags" => FILTER_NULL_ON_FAILURE]);
             if ($typeInt !== NULL && $typeInt < count(RecipeCategory::cases())) {
                 $type = RecipeCategory::cases()[$typeInt];
+                $whereClause = "category = ?";
+                $whereParams[] = $type->value;
+                $arr["type"] = $typeInt;
             } else {
                 throw new HTTPException(400);
             }
         }
 
-        if (isset($type)) {
-            $whereClause = "category = ?";
-            $whereParams[] = $type->value;
-        }
 
         $time = $this->request()->getValue('time');
-        if (isset($time)) {
+        //$time = $formData['time'] ?? null;
+        if (!empty($time)) {
             $time = (int)$time;
             if ($time > 0) {
                 if (!is_null($whereClause)) {
@@ -48,15 +53,16 @@ class HomeController extends AControllerBase
                 }
                 $whereClause .= "minutes <= ?";
                 $whereParams[] = $time;
+                $arr["time"] = $time;
 
             } else {
                 throw new HTTPException(400);
             }
         }
         $recipes = Recipe::getAll(whereClause: $whereClause, whereParams: $whereParams, orderBy: "RAND()", limit: 20);
+        $arr["recipes"] = $recipes;
 
-
-        return $this->html(['recipes' => $recipes]);
+        return $this->html($arr);
     }
 
 
--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -8,31 +8,34 @@
 
 <div class="container mt-5">
 
-    <div class="row mb-3 filter bg-secondary-subtle">
-        <div class="col-md-6 d-flex flex-column align-items-center justify-content-center">
-            <label for="filterType">Typ jedla:</label>
-            <select class="form-control " id="filterType">
-                <option value="all">Všetky</option>
-                <option value="slane">Slané</option>
-                <option value="sladke">Sladké</option>
-            </select>
+    <form method="get" action="<?= $link->url("index") ?>">
+        <div class="row mb-3 filter bg-secondary-subtle">
+            <div class="col-md-6 d-flex flex-column align-items-center justify-content-center">
+                <label for="filterType">Typ jedla:</label>
+                <select class="form-control" id="filterType" name="type">
+                    <option value="">Všetky</option>
+                    <option value="0" <?= isset($data["type"]) && @$data["type"] === 0 ? "selected" : "" ?>>Slané</option>
+                    <option value="1" <?= isset($data["type"]) && @$data["type"] === 1 ? "selected" : "" ?>>Sladké</option>
+                </select>
+            </div>
+            <div class="col-md-6 d-flex flex-column align-items-center justify-content-center">
+                <label for="filterTime">Čas prípravy:</label>
+                <select class="form-control" id="filterTime" name="time">
+                    <option value="">Všetky</option>
+                    <option value="15" <?= isset($data["time"]) && @$data["time"] === 15 ? "selected" : "" ?>>Do 15 minút</option>
+                    <option value="30" <?= isset($data["time"]) && @$data["time"] === 30 ? "selected" : "" ?>>Do 30 minút</option>
+                    <option value="60" <?= isset($data["time"]) && @$data["time"] === 60 ? "selected" : "" ?>>Do 1 hodiny</option>
+                </select>
+            </div>
         </div>
-        <div class="col-md-6 d-flex flex-column align-items-center justify-content-center">
-            <label for="filterTime">Čas prípravy:</label>
-            <select class="form-control" id="filterTime">
-                <option value="all">Všetky</option>
-                <option value="15">Do 15 minút</option>
-                <option value="30">Do 30 minút</option>
-                <option value="60">Do 1 hodiny</option>
-            </select>
-        </div>
-    </div>
 
-    <div class="row mb-3">
-        <div class="col-md-12 text-center">
-            <button class="btn btn-dark">Ďaľšie inšpirácie</button>
+        <div class="row mb-3">
+            <div class="col-md-12 text-center">
+                <button type="submit" class="btn btn-dark">Ďaľšie inšpirácie</button>
+            </div>
         </div>
-    </div>
+    </form>
+
 
 
     <div class="row mt-5 row-cols-1 row-cols-md-2 row-cols-xl-3">
